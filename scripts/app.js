36c4cc27fcf211e4085c9a01355d6f07fef26cca
pathiggins13 <pat_higgins@hotmail.com>
2015-10-21 21:23:07 -0600

    ready for bootstrap fix

--- scripts/app.js
+++ scripts/app.js
@@ -14,7 +14,27 @@ utahVotes.config(function($stateProvider, $urlRouterProvider) {
         })
 
         .state('about', {
-            // we'll get to this in a bit       
-        });
+            url: '/about',
+            templateUrl: 'partials/about.html'
+        })
 
+        .state('data', {
+            url: '/data',
+            templateUrl: 'partials/data.html'
+        })
+
+        .state('campaigns', {
+            url: '/campaigns',
+            templateUrl: 'partials/campaigns.html'
+        })
+
+        .state('contact', {
+            url: '/contact',
+            templateUrl: 'partials/contact.html'
+        })
+
+        .state('member', {
+            url: '/member',
+            templateUrl: 'partials/member.html'
+        })
 });
